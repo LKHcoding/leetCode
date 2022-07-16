3ed5c26e1ecfc4b2a6cc002ac26fea99ce04899c
lkhcoding <tha920@naver.com>
2022-07-16 23:52:00 +0900

    array101
    heightChecker,
    removeElement,
    sortArrayByParity,
    thirdMaximumNumber 풀이 완료

--- src/array101/removeElement.js
+++ src/array101/removeElement.js
@@ -12,22 +12,39 @@ var removeElement = function (nums, val) {
   // }
   // return nums.length;
 
+  // 이중포문 돌 필요 없음. 투포인터로 해결 가능.
+  // let numsLength = nums.length;
+  // const deleteAtIdx = (idx) => {
+  //   nums[idx] = null;
+  //   for (let i = idx; i < numsLength; i++) {
+  //     if (nums[i + 1] !== undefined) {
+  //       nums[i] = nums[i + 1];
+  //     }
+  //   }
+  //   numsLength--;
+  // };
+  // for (let idx = nums.length; idx >= 0; idx--) {
+  //   if (nums[idx] === val) {
+  //     deleteAtIdx(idx);
+  //   }
+  // }
+  // return numsLength;
+
   let numsLength = nums.length;
-  const deleteAtIdx = (idx) => {
-    nums[idx] = null;
-    for (let i = idx; i < numsLength; i++) {
-      if (nums[i + 1] !== undefined) {
-        nums[i] = nums[i + 1];
-      }
-    }
-    numsLength--;
-  };
-  for (let idx = nums.length; idx >= 0; idx--) {
-    if (nums[idx] === val) {
-      deleteAtIdx(idx);
+  let writeCursor = 0;
+
+  for (let idx = 0; idx < nums.length; idx++) {
+    if (val !== nums[idx]) {
+      nums[writeCursor] = nums[idx];
+      writeCursor++;
+    } else {
+      numsLength--;
     }
   }
+
   return numsLength;
 };
 
+// [0,1,2,2,3,0,4,2]
+// 2
 removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2);
